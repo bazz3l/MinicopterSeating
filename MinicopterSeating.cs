d7e67f0d154fb2a127e4a8226c95a696b0c4d698
Bazz3l <30571705+bazz3l@users.noreply.github.com>
2019-09-05 23:59:03 +0100

    Update MinicopterSeating.cs

--- MinicopterSeating.cs
+++ MinicopterSeating.cs
@@ -3,82 +3,49 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Minicopter Seating", "Bazz3l", "1.0.1")]
-    [Description("Adds 2 extra seats to minicopter")]
-    class MinicopterSeating : RustPlugin
+    [Info("Horse Seating", "Bazz3l", "1.0.1")]
+    [Description("Adds 2 seats to horse")]
+    class HorseSeating : RustPlugin
     {
-        void OnEntitySpawned(MiniCopter mini)
+        void OnEntitySpawned(RidableHorse horse)
         {
-            if (mini == null)
+            if (horse == null)
             {
                 return;
             }
 
-            mini.gameObject.AddComponent<CopterSeating>();
+            if(horse.transform.GetComponentsInChildren<Seating>().Length > 0)
+            {
+                return;
+            }
+
+            horse.gameObject.AddComponent<Seating>();
         }
 
-        class CopterSeating : MonoBehaviour
+        class Seating : MonoBehaviour
         {
-            public const string ChairPrefab = "assets/prefabs/vehicle/seats/passengerchair.prefab";
-            public BaseVehicle mini;
-            public BaseEntity seat1;
-            public BaseEntity seat2;
+            public BaseVehicle horse;
 
             void Awake()
             {
-                mini = GetComponent<BaseVehicle>();
+                horse = GetComponent<BaseVehicle>();
                 if (mini == null)
                 {
+                    Destroy(this);
                     return;
                 }
 
-                Array.Resize(ref mini.mountPoints, 4);
-
-                BaseVehicle.MountPointInfo mountPoint1 = new BaseVehicle.MountPointInfo
-                {
-                    pos       = new Vector3(0.6f, 0f, -0.2f),
-                    rot       = mini.mountPoints[0].rot,
-                    prefab    = mini.mountPoints[1].prefab,
-                    mountable = mini.mountPoints[0].mountable,
-                };
+                Array.Resize(ref horse.mountPoints, 2);
 
                 BaseVehicle.MountPointInfo mountPoint2 = new BaseVehicle.MountPointInfo
                 {
-                    pos       = new Vector3(-0.6f, 0f, -0.2f),
+                    pos       = new Vector3(0f, 0f, -0.2f),
                     rot       = mini.mountPoints[0].rot,
                     prefab    = mini.mountPoints[1].prefab,
                     mountable = mini.mountPoints[0].mountable,
                 };
 
-                mini.mountPoints[2] = mountPoint1;
-                mini.mountPoints[3] = mountPoint2;
-
-                seat1 = GameManager.server?.CreateEntity(ChairPrefab, mini.transform.position) as BaseEntity;
-                if (seat1 == null)
-                {
-                    return;
-                }
-
-                seat1.SetParent(mini);
-                seat1.Spawn();
-                seat1.transform.localPosition = new Vector3(0.6f, 0f, -0.4f);
-                seat1.SendNetworkUpdateImmediate(true);
-
-                seat2 = GameManager.server?.CreateEntity(ChairPrefab, mini.transform.position) as BaseEntity;
-                if (seat2 == null)
-                {
-                    return;
-                }
-
-                seat2.SetParent(mini);
-                seat2.Spawn();
-                seat2.transform.localPosition = new Vector3(-0.6f, 0f, -0.4f);
-                seat2.SendNetworkUpdateImmediate(true);
-            }
-
-            void OnDestroy()
-            {
-                Destroy(this);
+                horse.mountPoints[2] = mountPoint1;
             }
         }
     }
