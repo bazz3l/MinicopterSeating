5de475aebf081089819ebaf1240400c9d08f5a32
Bazz3l <30571705+bazz3l@users.noreply.github.com>
2021-07-15 15:05:47 +0100

    seat positions
    added tail seat
    seat positions are now configurable

--- MinicopterSeating.cs
+++ MinicopterSeating.cs
@@ -1,53 +1,127 @@
-using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
-using System;
 
 namespace Oxide.Plugins
 {
-    [Info("Minicopter Seating", "Bazz3l", "1.1.5")]
+    [Info("Minicopter Seating", "Bazz3l", "1.1.8")]
     [Description("Spawns an extra seat each side of the minicopter.")]
     class MinicopterSeating : RustPlugin
     {
         #region Fields
-        const string _chairPrefab = "assets/prefabs/vehicle/seats/passengerchair.prefab";
-        SeatingManager _manager = new SeatingManager();
+
+        private const string CHAIR_PREFAB = "assets/prefabs/vehicle/seats/passengerchair.prefab";
+        private readonly SeatingManager _manager = new SeatingManager();
+        private static PluginConfig _config;
+        
+        #endregion
+        
+        #region Config
+
+        protected override void LoadDefaultConfig() => _config = PluginConfig.DefaultConfig();
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+
+            try
+            {
+                _config = Config.ReadObject<PluginConfig>();
+
+                if (_config == null)
+                {
+                    throw new JsonException();
+                }
+            }
+            catch
+            {
+                PrintWarning("Default config loaded.");
+
+                LoadDefaultConfig();
+            }
+        }
+
+        protected override void SaveConfig() => Config.WriteObject(_config, true);
+        
+        private class PluginConfig
+        {
+            #region Fields
+
+            public bool EnableTail;
+            public bool EnableSide;
+            public Vector3 TailPosition;
+            public Vector3 LeftPosition;
+            public Vector3 RightPosition;
+
+            #endregion
+            
+            public static PluginConfig DefaultConfig()
+            {
+                return new PluginConfig
+                {
+                    EnableTail = false,
+                    EnableSide = true,
+                    TailPosition = new Vector3(0f, 0.4f, -1.5f),
+                    LeftPosition = new Vector3(-0.6f, 0.2f, -0.3f),
+                    RightPosition = new Vector3(0.6f, 0.2f, -0.3f)
+                };
+            }
+        }
+
         #endregion
 
         #region Oxide
-        void OnEntitySpawned(BaseVehicle mini)
+
+        private void Unload()
         {
-            if (mini.mountPoints.Count < 4 && mini.ShortPrefabName == "minicopter.entity")
+            _config = null;
+        }
+
+        private void OnEntitySpawned(BaseVehicle mini)
+        {
+            if (mini.mountPoints.Count == 2 && mini.ShortPrefabName == "minicopter.entity")
             {
                 _manager.Setup(mini);
             }
         }
+        
         #endregion
 
         #region Seating
-        class SeatingManager
+
+        private class SeatingManager
         {
             public void Setup(BaseVehicle vehicle)
             {
-                BaseVehicle.MountPointInfo pilot = vehicle.mountPoints[0];
-                BaseVehicle.MountPointInfo passenger = vehicle.mountPoints[1];
-
-                vehicle.mountPoints.Add(MakeMount(vehicle, new Vector3(0.6f, 0.2f, -0.3f)));
-                vehicle.mountPoints.Add(MakeMount(vehicle, new Vector3(-0.6f, 0.2f, -0.3f)));
-
-                MakeSeat(vehicle, new Vector3(0.6f, 0.2f, -0.5f));
-                MakeSeat(vehicle, new Vector3(-0.6f, 0.2f, -0.5f));
+                CreateSideSeating(vehicle);
+                CreateTailSeating(vehicle);
             }
 
-            void MakeSeat(BaseVehicle vehicle, Vector3 position)
+            private void CreateSideSeating(BaseVehicle vehicle)
             {
-                BaseEntity entity = GameManager.server.CreateEntity(_chairPrefab, vehicle.transform.position);
-                if (entity == null) return;
+                if (!_config.EnableSide) return;
+
+                CreateSeat(vehicle, _config.LeftPosition);
+                CreateSeat(vehicle, _config.RightPosition);
+            }
+            
+            private void CreateTailSeating(BaseVehicle vehicle)
+            {
+                if (!_config.EnableTail) return;
+                
+                CreateSeat(vehicle, _config.TailPosition);
+            }
+
+            private void CreateSeat(BaseVehicle vehicle, Vector3 position)
+            {
+                BaseEntity entity = GameManager.server.CreateEntity(CHAIR_PREFAB, vehicle.transform.position);
                 entity.SetParent(vehicle);
                 entity.transform.localPosition = position;
                 entity.Spawn();
+                
+                vehicle.mountPoints.Add(CreateMount(vehicle, position));
             }
 
-            BaseVehicle.MountPointInfo MakeMount(BaseVehicle vehicle, Vector3 position)
+            private BaseVehicle.MountPointInfo CreateMount(BaseVehicle vehicle, Vector3 position)
             {
                 return new BaseVehicle.MountPointInfo
                 {
@@ -58,6 +132,7 @@ namespace Oxide.Plugins
                 };
             }
         }
+        
         #endregion
     }
 }
